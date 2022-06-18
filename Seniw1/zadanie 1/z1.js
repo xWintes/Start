67e3092b2108e860429276e3c61dfcdf9e3ed908
Antoni <79842762+xWintes@users.noreply.github.com>
2022-06-18 11:47:16 +0200

    Add files via upload

---
+++ Seniw1/zadanie 1/z1.js
@@ -0,0 +1,15 @@
+var rys = true
+
+function zmiana()
+{
+    if (rys)
+{
+    document.obraz.src="b.jpg";
+    rys = false;
+}
+else 
+{
+document.obraz.src="a.jpg";
+rys = true;
+}
+}
---
+++ Seniw1/zadanie 4/z4.js
@@ -0,0 +1,11 @@
+if (confirm("Okno decyzyjne")) {
+    alert("Wybrano opcję : OK");
+    document.write("<font size=+2>");
+    document.write("Został kliknięty przycisk OK");
+    document.write("<br> Antoni Seniw - Zadanie 4");
+} else {
+    alert("Wybrano opcję : Anuluj");
+    document.write("<font size=+2>");
+    document.write("Został kliknięty przycisk : Anuluj");
+    document.write("<br> Antoni Seniw - Zadanie 4");
+}
